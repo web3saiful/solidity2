dcf6ef2671ccb6d2b4f8dd1dc768505b2c75410e
web3saiful <web3saiful@gmail.com>
2025-11-09 23:55:53 +0600

    'add'

---
+++ test.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.10;
+
+
+
+// Introduction to Solidity
+// contract MyFirstContract{
+//     uint256 public constant myInt=123;
+//     address public immutable myAddress;
+// }
+
+
+
+
+
+// // stroring values in Contract
+// contract First{
+
+// int public num=18;
+
+// string public greetng="welcome";
+
+// bool public isActive=false;
+
+// address public myAddress=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+// function getNumber()public view returns(int){
+//     return num;
+//    }
+// }
+
+
