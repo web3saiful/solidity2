cc8de72f941b6bb5e323594db180104e1534ad6b
web3saiful <web3saiful@gmail.com>
2025-11-11 13:42:17 +0600

    'Updated'

---
+++ Array.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Array{
+    // fixed size array
+    uint[3] public scores=[85,90,75];
+
+    uint[] public dynamicScores;
+
+    function addScores(uint score)public {
+        dynamicScores.push(score);  // add a new number to the end
+    }
+    function getScore(uint index) public view returns (uint){
+        return dynamicScores[index];
+    }
+    
+    function getNumberOfScores()public view returns  (uint){
+        return dynamicScores.length;
+    }
+
+    function removeLastScore()public {
+        dynamicScores.pop();
+    }
+}
