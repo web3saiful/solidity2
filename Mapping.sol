d075ff874a8844b3b07a6d5539493606d91f8b55
web3saiful <web3saiful@gmail.com>
2025-11-13 21:11:37 +0600

    'Updated'

--- Mapping.sol
+++ Mapping.sol
@@ -8,4 +8,8 @@ mapping(address=>mapping (address=>uint))public allowances;
 function updateBalance(uint newBalance)public {
     balances[msg.sender]=newBalance;
   }
+
+  function getBalance(address user)public view returns(uint){
+    return balances[user];
+  }
 }
