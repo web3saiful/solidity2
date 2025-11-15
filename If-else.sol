8a71d91c82f2d0554c22d949ad2aa16f567af22c
web3saiful <web3saiful@gmail.com>
2025-11-15 22:53:31 +0600

    'Updated'

--- If-else.sol
+++ If-else.sol
@@ -9,7 +9,12 @@ contract operator{
     function get() public view returns (uint){
         if(a==5 && b==4){
             return 1;
-
+        }else if(a==5){
+            return 3;
+        }else if(a==2){
+            return 4;
+        }else {
+            return 2;
         }
     }
 }
