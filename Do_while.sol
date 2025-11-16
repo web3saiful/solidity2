b1215e1986f04f29c301b86d9fac7e85fe89e2c7
web3saiful <web3saiful@gmail.com>
2025-11-16 12:23:51 +0600

    'Updated'

---
+++ Do_while.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract operator{
+    uint public a=5;
+    uint public b=10;
+
+    function get()public {
+        do{
+            a+=1;
+        }while(a<b);
+    }
+}
