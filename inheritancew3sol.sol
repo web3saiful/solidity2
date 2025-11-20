c2e1ac9cb4a32740ccb522b4abe128446e008d4a
web3saiful <web3saiful@gmail.com>
2025-11-20 15:18:54 +0600

    'Updated'

--- inheritancew3sol.sol
+++ inheritancew3sol.sol
@@ -1,12 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+// contract a {
+//     uint public num=1;
+// }
+
+// contract b is a{
+//     function getVal()public view returns(uint){
+//         return num;
+//     }
+// }
+
+
+
+
+
+
+
+
 contract a {
-    uint public num=1;
+    uint public num;
+
+    constructor(uint _num){
+        num=_num;
+    }
 }
 
-contract b is a{
+contract b is a(4){
     function getVal()public view returns(uint){
         return num;
     }
 }
+
+
