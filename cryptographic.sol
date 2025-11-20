4389b562e276bd8d379394b01514f8e2f6aefe04
web3saiful <web3saiful@gmail.com>
2025-11-20 14:33:58 +0600

    'Updated'

--- cryptographic.sol
+++ cryptographic.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract hashfunctions{
-     bytes32 public password;
+// contract hashfunctions{
+//      bytes32 public password;
     
-    function generatePass(string memory _pass)public {
-      password=keccak256(abi.encodePacked(_pass));
-    }
+//     function generatePass(string memory _pass)public {
+//       password=keccak256(abi.encodePacked(_pass));
+//     }
 
-    function sendMoney(string memory _pass)public view {
-        require(keccak256(abi.encodePacked(_pass))==password,"Wrong password");
+//     function sendMoney(string memory _pass)public view {
+//         require(keccak256(abi.encodePacked(_pass))==password,"Wrong password");
+//     }
+// }  
+
+
+
+
+
+contract hashfunctions{
+    
+    function generatePass(string memory _pass,string memory _name)public pure returns (bytes32) {
+   
+    return keccak256(abi.encode(_pass,_name)); 
+
     }
-}  
+    
+} 
