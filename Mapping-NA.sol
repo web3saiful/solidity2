fdc0fa1b7a32b0681f43c968ae6a9d457af818bd
web3saiful <web3saiful@gmail.com>
2025-11-21 12:43:47 +0600

    'Updated'

--- Mapping-NA.sol
+++ Mapping-NA.sol
@@ -2,15 +2,40 @@
 pragma solidity ^0.8.10;
 
 contract mappingDemo{
-    mapping (address=>uint256)public balances;
-    mapping (uint256=>string)public usernames;
-    mapping(string=>bool)public isValid;
+    //  simple Mapping
+    // mapping (address=>uint256)public balances;
+    // mapping (uint256=>string)public usernames;
+    // mapping(string=>bool)public isValid;
 
-    function setBalance(address user,uint256 amount)public {
-        balances[user]=amount;
-    }
+    // function setBalance(address user,uint256 amount)public {
+    //     balances[user]=amount;
+    // }
 
-    function getBalances(address user)public view returns(uint256){
-        return balances[user];
-    }
+    // function getBalances(address user)public view returns(uint256){
+    //     return balances[user];
+    // }
+
+
+
+
+
+
+
+
+
+
+
+mapping (address=>mapping(address=>uint256))public checkTokenBalanc;
+
+mapping (address=>mapping (uint256=>mapping (uint256=>uint256))) public checkScore;
+
+
+
+function settValue(uint256 gameId,uint level,uint256 score)public {
+    checkScore[msg.sender][gameId][level]=score;
+}
+
+
+
+
 }
