117839307be88ba0ef98632fb5058029ef95f495
web3saiful <web3saiful@gmail.com>
2025-11-22 17:27:35 +0600

    'Updated'

--- Struct&Enum_NA.sol
+++ Struct&Enum_NA.sol
@@ -17,6 +17,56 @@ struct Product{
     mapping (uint256=>Product)public productById;
 
 
+    enum OrderStatus{
+        Pending,
+        Confirmed,
+        Shipped,
+        Delivered,
+        Cancelled
+    }
+
+   struct Order{
+    uint256 id;
+    address buyer;
+    uint256 productId;
+    OrderStatus status;
+    uint256 timestamp;
+   }
+
+   mapping (uint256=>Order)public orders;
+
+   function createOrder(uint256 productId) public{
+    uint256 orderId=block.timestamp;
+
+    orders[orderId]=Order({
+        id:orderId,
+        buyer:msg.sender,
+        productId:productId,
+        status:OrderStatus.Pending,
+        timestamp:block.timestamp
+    });
+   }
+
+   function updateOrderStatus(uint256 orderId,OrderStatus newStatus)public {
+    require(orders[orderId].buyer==msg.sender,"This is not order");
+
+    OrderStatus currentStatus=orders[orderId].status;
+    require(isValidTransition(currentStatus,newStatus),"Invalid Transition");
+
+    orders[orderId].status=newStatus;
+
+   }
+
+   function isValidTransition(OrderStatus from,OrderStatus to)public pure returns(bool){
+    if(from==OrderStatus.Pending)return to==OrderStatus.Confirmed||to==OrderStatus.Cancelled;
+    if(from==OrderStatus.Confirmed)return to==OrderStatus.Shipped||to==OrderStatus.Cancelled;
+    if(from==OrderStatus.Shipped)return to==OrderStatus.Delivered;
+
+
+    return false;
+
+
+   }
 
     function addProduct(string memory name,uint256 price)public returns(uint256){
         uint256 productId=products.length; 
