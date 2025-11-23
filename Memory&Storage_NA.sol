43f8448bc8ff77b2ebd9dbd82b3807938132dfc5
web3saiful <web3saiful@gmail.com>
2025-11-23 13:32:15 +0600

    'update: ongoing learning & practice iterations'

--- Memory&Storage_NA.sol
+++ Memory&Storage_NA.sol
@@ -12,11 +12,55 @@ contract DataLocation{
     //     storageRef[0]=200;
     // }
  
- 
-     function workWithMemory()public pure{
-        uint256[]memory tempNumbers=new uint256[](5);
 
-        tempNumbers[0]=100;
-     }
+
+
+
+
+
+
+
+    //  function workWithMemory()public pure{
+    //     uint256[]memory tempNumbers=new uint256[](5);
+
+    //     tempNumbers[0]=100;
+    //  }
+
+
+
+
+
+
+
+
+    // calldata->Read only input data
+
+    function processCallData(uint256[] calldata inputNumbers)external pure returns(uint256){
+        return inputNumbers[0];
+    }
+ 
+
+
+
+
+
+
+
+function fun1(uint256[] storage arr)internal {
+    arr[0]=999;
+}
+
+function fun2(uint256[] memory arr)internal  pure{
+    arr[0]=999;
+}
+
+
+
+
+
+
+
+
+
 
 }
