1cebdab577feab16bbfc9ee887cfc00fbc1f328f
web3saiful <web3saiful@gmail.com>
2025-11-23 22:34:55 +0600

    'update: ongoing learning & practice iterations'

--- Bank_create_NA.sol
+++ Bank_create_NA.sol
@@ -7,11 +7,22 @@ contract BankAccount{
     uint256 public totalDeposits;
 
 
-    event Doposte(address indexed account, uint256 amount); //3 indexed
+    event Deposit(address indexed account, uint256 amount); //3 indexed
     event Withdraw(address account,uint256 amount);
 
 
+function deposit()external payable {
+    require(msg.value>0,'Must deposit something');
 
-
+    if(balances[msg.sender]==0){
+        accountHolders.push(msg.sender);
     }
+
+    balances[msg.sender]+=msg.value;
+    totalDeposits+=msg.value;
+
+    emit Deposit(msg.sender,msg.value);
+  }
+
+
 }
