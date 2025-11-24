fe7bbd6e482eab4e3f7431142e292e57b0a156d9
web3saiful <web3saiful@gmail.com>
2025-11-24 23:42:24 +0600

    'update: ongoing learning & practice iterations'

--- Funtion_modifier_NA.sol
+++ Funtion_modifier_NA.sol
@@ -1,29 +1,73 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract Demo{
-    uint public num;
-    address public owner;
+// contract Demo{
+//     uint public num;
+//     address public owner;
 
+
+//     modifier onlyOwner(){
+//         require(owner==msg.sender,"You are not allowed");
+//         _;//placeholder
+//     }
+
+//     function fun1()public view onlyOwner{
+     
+//     }
+
+//     function fun2()public view onlyOwner{
+     
+//     }    
+
+
+//     function fun3()public view onlyOwner{
+     
+//     }
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+contract Demo{
+    address public owner;
+    bool public paused=false;
+
+    constructor(){
+        owner=msg.sender;
+    }
 
     modifier onlyOwner(){
-        require(owner==msg.sender,"You are not allowed");
-        _;//placeholder
+        require(msg.sender==owner,"You are not owner");
+        _;
     }
 
-    function fun1()public view onlyOwner{
-     
+     modifier whenNotPaused(){
+        require(!paused,"Contract is paused");
+        _;
+     }
+
+     function changeOwner(address newOwner)public onlyOwner{
+        owner=newOwner;
+     }
+
+    function emergencyPause()public onlyOwner{
+        paused=true;
     }
-
-    function fun2()public view onlyOwner{
-     
-    }    
-
-
-    function fun3()public view onlyOwner{
-     
-    }
-
-
     
+    function withdraw()public onlyOwner whenNotPaused{
+        payable (owner).transfer(address(this).balance);
+    }
+
 }
+
+
