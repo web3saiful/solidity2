1f4cfd5c3bec95df7c8ceddc7c70ea2c5275283e
web3saiful <web3saiful@gmail.com>
2025-11-26 22:36:26 +0600

    "update: ongoing learning & practice iterations"

---
+++ Interface1.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IToken{
+    //declare function without implementation
+    function getBalance(address account)external view returns(uint256);
+    function transfer(address to ,uint amount)external returns(bool);
+    function mint(address to, uint256 amount)external;
+
+}
+
+contract MyToken is IToken{
+    mapping (address=> uint256)private balances;
+    address public owner;
+
+    constructor(){
+        owner=msg.sender;
+    }
+ 
+    function getBalance(address account)external view override returns(uint256){
+     return balances[account];
+    }
+
+    function transfer(address to,uint256 amount)external override returns(bool){
+        require(balances[msg.sender]>=amount,"Insufficient balance");
+        balances[msg.sender]-=amount;
+        balances[to]+=amount;
+
+        return true;
+    }
+
+
+}
+
