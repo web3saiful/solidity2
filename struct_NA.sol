5d488cb58d7ac558d643e076ad509ae8c34811b4
web3saiful <web3saiful@gmail.com>
2025-11-27 22:59:57 +0600

    "update: ongoing learning & practice iterations"

---
+++ struct_NA.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+contract StructExample{
+    struct Person{
+        string name;
+        uint age;
+        address walletAddress;
+        bool isActive;
+    }
+    //string public name
+    Person public person1;
+    Person public person2;
+    function setPerson(string memory _name,uint _age,address _walletAddress)public{
+        person1=Person(_name,_age,_walletAddress,true);
+    }
+
+}
